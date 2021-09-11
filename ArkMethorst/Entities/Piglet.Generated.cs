3cc9088155de75be585c15fb51ebca6113e90d3e
Michael Whitaker <whitaker.mj@outlook.com>
2021-09-11 14:08:30 +0200

    Added piglet class

---
+++ ArkMethorst/Entities/Piglet.Generated.cs
@@ -0,0 +1,188 @@
+#if ANDROID || IOS || DESKTOP_GL
+#define REQUIRES_PRIMARY_THREAD_LOADING
+#endif
+using Color = Microsoft.Xna.Framework.Color;
+using System.Linq;
+using FlatRedBall.Graphics;
+using FlatRedBall.Math;
+using FlatRedBall;
+using System;
+using System.Collections.Generic;
+using System.Text;
+namespace ArkMethorst.Entities
+{
+    public partial class Piglet : ArkMethorst.Entities.Animal, FlatRedBall.Graphics.IDestroyable
+    {
+        // This is made static so that static lazy-loaded content can access it.
+        public static new string ContentManagerName
+        {
+            get
+            {
+                return ArkMethorst.Entities.Animal.ContentManagerName;
+            }
+            set
+            {
+                ArkMethorst.Entities.Animal.ContentManagerName = value;
+            }
+        }
+        #if DEBUG
+        static bool HasBeenLoadedWithGlobalContentManager = false;
+        #endif
+        static object mLockObject = new object();
+        static System.Collections.Generic.List<string> mRegisteredUnloads = new System.Collections.Generic.List<string>();
+        static System.Collections.Generic.List<string> LoadedContentManagers = new System.Collections.Generic.List<string>();
+        
+        public Piglet () 
+        	: this(FlatRedBall.Screens.ScreenManager.CurrentScreen.ContentManagerName, true)
+        {
+        }
+        public Piglet (string contentManagerName) 
+        	: this(contentManagerName, true)
+        {
+        }
+        public Piglet (string contentManagerName, bool addToManagers) 
+        	: base(contentManagerName, addToManagers)
+        {
+            ContentManagerName = contentManagerName;
+        }
+        protected override void InitializeEntity (bool addToManagers) 
+        {
+            LoadStaticContent(ContentManagerName);
+            
+            base.InitializeEntity(addToManagers);
+        }
+        public override void ReAddToManagers (FlatRedBall.Graphics.Layer layerToAddTo) 
+        {
+            base.ReAddToManagers(layerToAddTo);
+        }
+        public override void AddToManagers (FlatRedBall.Graphics.Layer layerToAddTo) 
+        {
+            LayerProvidedByContainer = layerToAddTo;
+            base.AddToManagers(layerToAddTo);
+            CustomInitialize();
+        }
+        public override void Activity () 
+        {
+            base.Activity();
+            
+            CustomActivity();
+        }
+        public override void Destroy () 
+        {
+            base.Destroy();
+            
+            CustomDestroy();
+        }
+        public override void PostInitialize () 
+        {
+            bool oldShapeManagerSuppressAdd = FlatRedBall.Math.Geometry.ShapeManager.SuppressAddingOnVisibilityTrue;
+            FlatRedBall.Math.Geometry.ShapeManager.SuppressAddingOnVisibilityTrue = true;
+            base.PostInitialize();
+            FlatRedBall.Math.Geometry.ShapeManager.SuppressAddingOnVisibilityTrue = oldShapeManagerSuppressAdd;
+        }
+        public override void AddToManagersBottomUp (FlatRedBall.Graphics.Layer layerToAddTo) 
+        {
+            base.AddToManagersBottomUp(layerToAddTo);
+        }
+        public override void RemoveFromManagers () 
+        {
+            base.RemoveFromManagers();
+        }
+        public override void AssignCustomVariables (bool callOnContainedElements) 
+        {
+            base.AssignCustomVariables(callOnContainedElements);
+            if (callOnContainedElements)
+            {
+            }
+        }
+        public override void ConvertToManuallyUpdated () 
+        {
+            base.ConvertToManuallyUpdated();
+            this.ForceUpdateDependenciesDeep();
+            FlatRedBall.SpriteManager.ConvertToManuallyUpdated(this);
+        }
+        public static new void LoadStaticContent (string contentManagerName) 
+        {
+            if (string.IsNullOrEmpty(contentManagerName))
+            {
+                throw new System.ArgumentException("contentManagerName cannot be empty or null");
+            }
+            ContentManagerName = contentManagerName;
+            ArkMethorst.Entities.Animal.LoadStaticContent(contentManagerName);
+            // Set the content manager for Gum
+            var contentManagerWrapper = new FlatRedBall.Gum.ContentManagerWrapper();
+            contentManagerWrapper.ContentManagerName = contentManagerName;
+            RenderingLibrary.Content.LoaderManager.Self.ContentLoader = contentManagerWrapper;
+            // Access the GumProject just in case it's async loaded
+            var throwaway = GlobalContent.GumProject;
+            #if DEBUG
+            if (contentManagerName == FlatRedBall.FlatRedBallServices.GlobalContentManager)
+            {
+                HasBeenLoadedWithGlobalContentManager = true;
+            }
+            else if (HasBeenLoadedWithGlobalContentManager)
+            {
+                throw new System.Exception("This type has been loaded with a Global content manager, then loaded with a non-global.  This can lead to a lot of bugs");
+            }
+            #endif
+            bool registerUnload = false;
+            if (LoadedContentManagers.Contains(contentManagerName) == false)
+            {
+                LoadedContentManagers.Add(contentManagerName);
+                lock (mLockObject)
+                {
+                    if (!mRegisteredUnloads.Contains(ContentManagerName) && ContentManagerName != FlatRedBall.FlatRedBallServices.GlobalContentManager)
+                    {
+                        FlatRedBall.FlatRedBallServices.GetContentManagerByName(ContentManagerName).AddUnloadMethod("PigletStaticUnload", UnloadStaticContent);
+                        mRegisteredUnloads.Add(ContentManagerName);
+                    }
+                }
+            }
+            if (registerUnload && ContentManagerName != FlatRedBall.FlatRedBallServices.GlobalContentManager)
+            {
+                lock (mLockObject)
+                {
+                    if (!mRegisteredUnloads.Contains(ContentManagerName) && ContentManagerName != FlatRedBall.FlatRedBallServices.GlobalContentManager)
+                    {
+                        FlatRedBall.FlatRedBallServices.GetContentManagerByName(ContentManagerName).AddUnloadMethod("PigletStaticUnload", UnloadStaticContent);
+                        mRegisteredUnloads.Add(ContentManagerName);
+                    }
+                }
+            }
+            CustomLoadStaticContent(contentManagerName);
+        }
+        public static new void UnloadStaticContent () 
+        {
+            if (LoadedContentManagers.Count != 0)
+            {
+                LoadedContentManagers.RemoveAt(0);
+                mRegisteredUnloads.RemoveAt(0);
+            }
+            if (LoadedContentManagers.Count == 0)
+            {
+            }
+        }
+        [System.Obsolete("Use GetFile instead")]
+        public static new object GetStaticMember (string memberName) 
+        {
+            return null;
+        }
+        public static new object GetFile (string memberName) 
+        {
+            return null;
+        }
+        object GetMember (string memberName) 
+        {
+            return null;
+        }
+        public override void SetToIgnorePausing () 
+        {
+            base.SetToIgnorePausing();
+        }
+        public override void MoveToLayer (FlatRedBall.Graphics.Layer layerToMoveTo) 
+        {
+            var layerToRemoveFrom = LayerProvidedByContainer; // assign before calling base so removal is not impacted by base call
+            base.MoveToLayer(layerToMoveTo);
+        }
+    }
+}
